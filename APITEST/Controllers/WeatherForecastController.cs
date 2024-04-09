596a13202372faa76d858865581be1230e987da2
DevChristianMorris <devchristianmorris@gmail.com>
2024-04-09 14:01:34 +1200

    Changed weather temperature types

--- APITEST/Controllers/WeatherForecastController.cs
+++ APITEST/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace APITEST.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Middle", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
